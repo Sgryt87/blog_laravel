776a01d973ece10d1b9dc5afa3cf1169570a1f5a
Sergii <sergiigrytsaienko@gmail.com>
2018-01-24 17:34:29 -0600

    added blog.index pagination(10) to display all posts.

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -17,19 +17,21 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li class="{{ Request::is('/') ? 'active' : '' }}"><a href="{{url('/')}}">Home</a></li>
-                <li class="{{ Request::is('/about') ? 'active' : '' }}"><a href="{{url('/about')}}">About</a></li>
-                <li class="{{ Request::is('/contact') ? 'active' : '' }}"><a href="{{url('/contact')}}">Contact</a></li>
+                <li class="{{ Request::is('blog') ? 'active' : '' }}"><a href="{{url('/blog')}}">Blog</a></li>
+                <li class="{{ Request::is('about') ? 'active' : '' }}"><a href="{{url('/about')}}">About</a></li>
+                <li class="{{ Request::is('contact') ? 'active' : '' }}"><a href="{{url('/contact')}}">Contact</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
                        aria-expanded="false">My account <span class="caret"></span></a>
                     <ul class="dropdown-menu">
+                        <li><a href="{{route('posts.index')}}">Posts</a></li>
                         <li><a href="#">Action</a></li>
                         <li><a href="#">Another action</a></li>
                         <li><a href="#">Something else here</a></li>
                         <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
+                        <li><a href="#">Logout</a></li>
                     </ul>
                 </li>
             </ul>
